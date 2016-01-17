36a1af12ba39b9e3dbcbb5d9d4a1c2996d155322
Chris Borckholder <borckholder@gmail.com>
2016-01-17 21:40:47 +0100

    CmdLet: Invoke-ExpressionAt // Spread-Expression
    Executes an expression at a given location, failing if moving to that
    location is not possible. Inspects $? and $LASTEXITCODE to determine if
    the expression succeeded and generates a non-terminating error if not.
    Target path(s) may be provided as cli argument or via the pipeline.
    
    Spread-Expression reads target paths from a line-based descriptor file
    (default './.spread'). Each expression received as pipeline input is
    executed in each of the targetted paths.

---
+++ pyps/Invoke-ExpressionAt.Tests.ps1
@@ -0,0 +1,143 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+# save invocation location to restore it after test runs
+$origin = Get-Location
+# transient folder to be used in tests
+$cwd = Join-Path (Resolve-Path $env:TEMP) ([system.guid]::NewGuid().ToString())
+
+Describe "Invoke-ExpressionAt" {
+
+    BeforeEach {
+        $Error.Clear()
+        Set-Location $origin
+        Remove-Item -Path $cwd -Recurse -ErrorAction SilentlyContinue
+        New-Item -ItemType Directory -Path $cwd -Force
+    }
+    
+    AfterEach {
+        Set-Location $origin
+        Remove-Item -Path $cwd -Recurse
+    }
+
+    It "accepts named parameters" {
+        Invoke-ExpressionAt -Path $cwd -Command "Get-Host"
+    }
+
+    It "executes the given command" {
+        Invoke-ExpressionAt -Path $cwd Write-Output argument | Should Be "argument"
+    }
+
+    It "sets execution status on successful command" {
+        Invoke-ExpressionAt -Path $cwd Get-Location
+        $? | Should Be True
+    }
+    
+    It "propagates failure of commands" {
+        { Invoke-ExpressionAt -ErrorAction Stop -Path $cwd Get-Item /notthere } | Should Throw "Cannot find path"
+    }
+
+    Context "Legacy Commands" {
+        It "can execute legacy commands" {
+            Invoke-ExpressionAt -Path $cwd cmd.exe /c "echo test" | Should Be "test"
+        }
+
+        It "propagates success exit code of legacy commands" {
+            Invoke-ExpressionAt -Path $cwd cmd.exe /c EXIT /B 0
+            $? | Should Be True
+        }
+
+        It "propagates failure of legacy commands" {
+            { Invoke-ExpressionAt -ErrorAction Stop -Path $cwd cmd.exe /c EXIT /B 1 } | Should Throw "exit code 1"
+        }
+    }
+
+    Context "Locations" {
+        It "changes directory" {
+            (Invoke-ExpressionAt -Path $cwd Get-Location).Path | Should Be (Get-Item $cwd).FullName
+        }
+
+        It "returns to initial directory" {
+            $initial = Get-Location
+            Invoke-ExpressionAt -Path $cwd Get-Host
+            $(Get-Location).Path | Should Be $initial.Path
+        }
+
+        It "returns to initial directory even if command throws" {
+            $initial = Get-Location
+            { Invoke-ExpressionAt -Path $cwd "throw 'test error'" } | Should Throw "test error"
+            $(Get-Location).Path | Should Be $initial.Path
+        }
+
+        It "fails if target location is not accessible" {
+            { Invoke-ExpressionAt -ErrorAction Stop -Path /not-existing Get-Location } | Should Throw "Cannot find path"
+        }
+    }
+
+    Context "piped paths" {
+        It "executes command once for each piped path" {
+            function PipePathsCommand {}
+            Mock PipePathsCommand {}
+            $cwd,$cwd,$cwd  | Invoke-ExpressionAt -Command PipePathsCommand
+            Assert-MockCalled PipePathsCommand -Times 3 -Exactly
+        }
+
+        It "skips missing paths" {
+            function SkipPathCommand {}
+            Mock SkipPathCommand {}
+            $cwd,"/notthere",$cwd  | Invoke-ExpressionAt -Command SkipPathCommand
+            Assert-MockCalled SkipPathCommand -Times 2 -Exactly
+            $Error[0] | Should Match "Cannot find Path"
+        }
+
+        It "accepts PSPath from piped gci" {
+            "one","two" | ForEach-Object { New-Item -ItemType Directory -Path "$cwd/$_" -Force }
+            Get-ChildItem $cwd | Invoke-ExpressionAt -Command Get-Location | Should Exist
+        }
+    }
+
+    Context "paths as list" {
+        It "executes command once for each given path" {
+            function ListPathsCommand {}
+            Mock ListPathsCommand {}
+            Invoke-ExpressionAt -Path $cwd,$cwd,$cwd -Command ListPathsCommand
+            Assert-MockCalled ListPathsCommand -Times 3 -Exactly
+        }
+
+        It "skips missing paths" {
+            function ListSkipPathCommand {}
+            Mock ListSkipPathCommand {}
+            Invoke-ExpressionAt -Path $cwd,"/notthere",$cwd -Command ListSkipPathCommand
+            Assert-MockCalled ListSkipPathCommand -Times 2 -Exactly
+            $Error[0] | Should Match "Cannot find Path"
+        }
+    }
+
+    
+    Context "Spread-Expression" {
+        It "fails if no .spread found" {
+            { Spread-Expression Get-Location } | Should Throw "Cannot find path"
+        }
+
+        It "executes command in each path defined in .spread" {
+            $config = $(Join-Path $cwd ".spread")
+            $actual = $(Join-Path $cwd "result")
+            $(Join-Path $cwd "one"),$(Join-Path $cwd "two") | Out-File $config
+            Get-Content $config | ForEach-Object { New-Item -ItemType Directory -Path $_ -Force }
+            $(Spread-Expression -PathFile $config Get-Item .).Name | Out-File $actual
+            $actual | Should Contain "one"
+            $actual | Should Contain "two"
+        }
+
+        It "executes all commands from pipeline" {
+            $config= $(Join-Path $cwd ".spread")
+            $actual = $(Join-Path $cwd "result")
+            $cwd | Out-File $config
+            Get-Content $config | ForEach-Object { New-Item -ItemType Directory -Path $_ -Force }
+            "Write-Output test","Write-Output more" | Spread-Expression -PathFile $config | Out-File $actual
+            $actual | Should Contain "test"
+            $actual | Should Contain "more"
+        }
+    }
+}
---
+++ pyps/Invoke-ExpressionAt.ps1
@@ -0,0 +1,86 @@
+﻿<#
+.SYNOPSIS
+    Execute shell expressions in different locations.
+#>
+
+New-Alias spread Spread-Expression
+
+function Spread-Expression {
+    <#
+    .SYNOPSIS
+        Invoke given expression in multiple paths.
+    .DESCRIPTION
+        By default the target paths are read from a local ./.spread file. 
+        Each line should contain a target path. The expression is created by
+        concatenating all positional arguments. Alternatively expressions
+        may be passed through the pipeline.
+    .PARAMETER Command
+        The expression to run.
+    .PARAMETER PathFile
+        Override the source file for target paths.
+    #>
+    [CmdletBinding()]
+    Param([Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromRemainingArguments=$true)][string]$Command,
+          [Parameter(Mandatory=$false)][string]$PathFile = './.spread')
+
+    Begin {
+        $Paths = $(Get-Content -ErrorAction Stop $PathFile)
+    }
+
+    Process {
+        $Paths | Invoke-ExpressionAt -Command $Command
+    }
+}
+
+function Invoke-ExpressionAt {
+    <#
+    .SYNOPSIS
+        Run an expression in a given path.
+    .DESCRIPTION
+        The expression is only run if the target path actually exists.
+        If multiple paths are specified, the expression is run sequentially
+        in each of them.
+        The expressions's execution status and return code is inspected to
+        determine whether it succeeded. If a failure is detected,
+        a non-terminating error is raised.
+    .PARAMETER Path
+        The path where the expression is run.
+    .PARAMETER Command
+        The expression to run.
+    #>
+    [CmdletBinding()]
+    Param([Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)][alias("PSPath")][string[]]$Path,
+          [Parameter(Mandatory=$true, Position=0, ValueFromRemainingArguments=$true)][string]$Command)
+
+    Process {
+        $Path | ForEach-Object { __HandleSingleExpression $_ $Command }
+    }
+}
+
+function __HandleSingleExpression {
+    [CmdletBinding()]
+    Param([string]$Path, [string]$Command)
+
+    Write-Verbose "invoke $Path/$Command"
+    Push-Location $Path -StackName invoke
+    if ($?) {
+        try {
+            __ExecuteExpression $Command
+        } finally {
+            Pop-Location -StackName invoke
+        }
+    } else {
+        Write-Verbose "skipped $Path/$Command"
+    }
+}
+
+function __ExecuteExpression {
+    [CmdletBinding()]
+    Param([string]$It)
+
+    $Global:LASTEXITCODE = $null
+    Invoke-Expression $It
+    if ($Global:LASTEXITCODE) {
+        Write-Error -Category OperationStopped -CategoryActivity "Invocation" -TargetObject $It "$(Get-Location).Path/$It failed (exit code $LASTEXITCODE)"
+    }
+}
