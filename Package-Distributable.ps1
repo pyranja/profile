defb8f00bbcbb3b84c47b69537fb747529d224d1
Chris Borckholder <borckholder@gmail.com>
2016-01-23 00:38:34 +0100

    Keep powershell module in a nested folder
    Refactor packaging script. Include project-skeleton as assets resources in
    distributable.

--- Package-Distributable.ps1
+++ Package-Distributable.ps1
@@ -3,49 +3,50 @@
     Package the pyps module
 #>
 [CmdletBinding()]
-Param([Parameter(Mandatory = $true)][version]$ModuleVersion)
+Param([Parameter(Mandatory = $true)][version]$Version)
 
-$ModuleName = 'py-profile'
+$PackageName = 'py-profile'
 
-Write-Host "packaging $ModuleName-$moduleVersion"
+Write-Host "packaging $PackageName-$Version"
 
 $base = (Split-Path $PSCommandPath)
 $cwd = (Join-Path $base "dist")
-$src = (Join-Path $base "pyps")
-$cfg = (Join-Path $base "dotfiles")
-
-$dist = (Join-Path $cwd "$ModuleName")
-If (Test-Path $dist) {
-    Remove-Item $dist -Force -Recurse
+$assembly = (Join-Path $cwd "$PackageName")
+If (Test-Path $assembly) {
+    Remove-Item $assembly -Force -Recurse
 }
-New-Item $dist -ItemType Directory | Out-Null
+New-Item $assembly -ItemType Directory | Out-Null
 
-$manifestFileName = Join-Path $dist "$ModuleName.psd1"
+# Assemble powershell module
+$ModuleName = 'py-ps'
+$ModuleSources = (Join-Path $base "py-ps")
+
+Copy-Item -Path $ModuleSources -Destination $assembly -Recurse -Exclude *.Tests.*
 
 New-ModuleManifest `
-    -Path $manifestFileName `
-    -ModuleVersion $ModuleVersion `
+    -Path (Join-Path $assembly "$ModuleName\$ModuleName.psd1") `
+    -ModuleVersion $Version `
     -Guid 6d07c16c-3181-4844-9f4b-4d8a6a644634 `
     -Author 'Chris Borckholder' `
     -Copyright 'This is free and unencumbered software released into the public domain.' `
     -Description 'Personal dotfiles and utilities.' `
     -PowerShellVersion '3.0' `
-    -NestedModules (Get-ChildItem $src -Exclude *.psd1,*.Tests.* | % { $_.Name }) `
+    -NestedModules (Get-ChildItem $ModuleSources -Exclude *.psd1,*.Tests.* | % { $_.Name }) `
     -CmdletsToExport *-* `
     -FunctionsToExport *-*
 
-# Copy the distributable files to the dist folder.
-Get-ChildItem $src -Recurse -Exclude *.Tests.* | Copy-Item -Destination $dist -Recurse
-Copy-Item -Path $cfg -Destination $dist -Recurse
+# Copy non-shell resources
+$Assets = "dotfiles","project-skeleton" | foreach { Join-Path -Path $base -ChildPath $_ }
+Copy-Item -Path $Assets -Destination $assembly -Recurse
 
-# Requires .NET 4.5
+# Create distributable archive (requires .NET 4.5)
 Add-Type -AssemblyName "System.IO.Compression.FileSystem"
-$zipFileName = (Join-Path $cwd "$ModuleName-$ModuleVersion.zip")
+$zipFileName = (Join-Path $cwd "$PackageName-$Version.zip")
 If (test-path $zipFileName) {
     Remove-Item $zipFileName -Force
 }
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
 $includeBaseDirectory = $true
-[System.IO.Compression.ZipFile]::CreateFromDirectory($dist, $zipFileName, $compressionLevel, $includeBaseDirectory)
+[System.IO.Compression.ZipFile]::CreateFromDirectory($assembly, $zipFileName, $compressionLevel, $includeBaseDirectory)
 
 Write-Host "packaged as $zipFileName"
