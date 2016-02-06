9b63b484d5f740fb4235d5979496dc7ef3f3c565
Chris Borckholder <borckholder@gmail.com>
2016-02-07 00:40:08 +0100

    Initialize-Project Cmdlet
    Prepares a folder to be used as git-based project, using a default set of
    basic config files
    
     * .editorconfig
     * .gitignore
     * .gitattributes
    
    Closes #12

---
+++ py-ps/Initialize-Project.Tests.ps1
@@ -0,0 +1,43 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Initialize-Project" {
+
+    AfterEach {
+        Remove-Item -Force -Recurse TestDrive:\project
+    }
+
+    It "creates target location if necessary" {
+        Initialize-Project -Path TestDrive:\project
+
+        Test-Path TestDrive:\project -PathType Container | Should Be $true
+    }
+
+    It "works in existing folder" {
+        New-Item -ItemType Container TestDrive:\project
+
+        { Initialize-Project -Path TestDrive:\project } | Should Not Throw
+    }
+
+    It "creates a git repository" {
+        Initialize-Project -Path TestDrive:\project
+        
+        "TestDrive:\project\.git" | Should Exist
+    }
+
+    It "works if git repository is already existing" {
+        New-Item -ItemType Container TestDrive:\project
+        Push-Location TestDrive:\project
+        git init
+        Pop-Location
+
+        { Initialize-Project -Path TestDrive:\project } | Should Not Throw
+    }
+
+    It "creates generic config files" {
+        Initialize-Project -Path TestDrive:\project
+
+        @(".gitattributes", ".gitignore", ".editorconfig") | ForEach-Object { "TestDrive:\project\$_" } | Should Exist
+    }
+}
---
+++ py-ps/Initialize-Project.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+    Project bootstrapping.
+#>
+
+# project skeleton is installed besides module
+$ProjectSkeletonLocation = $(Join-Path $(Split-Path -Parent $PSCommandPath) project-skeleton)
+
+function Initialize-Project {
+    <#
+    .SYNOPSIS
+        Create a git-based project at target location.
+    .DESCRIPTION
+        Sets up the git repository and imports basic config files.
+    #>
+    [CmdletBinding()]
+    Param([Parameter(Mandatory=$false)][string]$Path = $(Get-Location))
+
+    If (-not $(Test-Path $Path)) {
+        New-Item -ItemType Directory $Path
+    }
+
+    Push-Location -StackName __Initialize-Project__ $Path
+    try {
+        git init
+        Copy-Item -Recurse "$ProjectSkeletonLocation\*"
+        git add .
+    } finally {
+        Pop-Location -StackName __Initialize-Project__
+    }
+}
