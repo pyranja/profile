a5b9345fb2d89e68335127b808b320fb67efaed5
Chrs Borckholder <chris.borckholder@zuehlke.com>
2017-07-12 14:13:12 +0200

    Split profile and module artifacts
    Assemble and publish .nupkg and module separately. Profile nupkg targets
    the bintray nuget feed and the module is pushed to PSGallery.

--- profile.build.ps1
+++ profile.build.ps1
@@ -5,15 +5,20 @@
 
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $false)][Version]$version = "0.0.0"
+    [Parameter(Mandatory = $false)][Version]$Version = "0.0.0",
+    [Parameter(Mandatory = $false)][string]$PsGalleryApiKey = $Env:PSGALLERY_API_KEY,
+    [Parameter(Mandatory = $false)][string]$BintrayApiKey = $Env:BINTRAY_API_KEY,
+    [switch]$ForcePublish = $false
 )
 
 #Requires -Version 5
 Set-StrictMode -Version "Latest"
 
 $workspace = $(Join-Path $PSScriptRoot "dist")
+$module_name = 'py-ps'
+$package_name = 'py-profile'
 
-task . Assemble, Test
+task . Test, Assemble
 
 # Synopsis: clear build output
 task Clean {
@@ -25,22 +30,26 @@ task Init {
     New-Item -Path $workspace -ItemType Directory -Force | Out-Null
 }
 
-# Synopsis: assemble py-ps powershell module
-task Assemble-PyPs Init, {
-    $module_name = 'py-ps'
+$module_artifact = "$workspace/py-ps.$Version.zip"
+
+# Synopsis: assemble py-ps powershell module and create a release zip ball
+task Assemble-PyPs -Inputs { Get-ChildItem -Path $module_name -Recurse -File } -Outputs { $module_artifact } Init, {
     $target = Join-Path -Path $workspace -ChildPath $module_name
     EnsureExistsAndEmpty $target
 
-    Get-ChildItem -Path $(Join-Path $PSScriptRoot $module_name) -Exclude *.Tests.* | Copy-Item -Recurse -Destination "$target"
+    Get-ChildItem -Path $module_name -Exclude *.Tests.* | Copy-Item -Recurse -Destination "$target"
 
     New-ModuleManifest `
         -Path $(Join-Path $target "$module_name.psd1") `
-        -ModuleVersion $version `
+        -ModuleVersion $Version `
         -Guid 6d07c16c-3181-4844-9f4b-4d8a6a644634 `
         -Author 'Chris Borckholder' `
         -CompanyName 'Personal' `
         -Copyright 'This is free and unencumbered software released into the public domain.' `
         -Description 'Personal dotfiles and utilities.' `
+        -Tags 'pyranja','dotfiles','personal' `
+        -ProjectUri 'https://github.com/pyranja/profile' `
+        -LicenseUri 'https://github.com/pyranja/profile/blob/master/LICENSE' `
         -PowerShellVersion '5.0' `
         -DefaultCommandPrefix 'Py' `
         -NestedModules $(Get-ChildItem "$target" -Recurse -Include *.ps1 | ForEach-Object { $_.Name }) `
@@ -48,25 +57,53 @@ task Assemble-PyPs Init, {
         -FunctionsToExport *-* `
         -AliasesToExport '__none' `
         -VariablesToExport '__none'
+
+    Compress-Archive -Path $target -DestinationPath $module_artifact -CompressionLevel Optimal -Force
 }
 
-# Synopsis: assemble profile nuget package
-task Assemble-Profile Init, {
-    $package_name = 'py-profile'
+# Synopsis: push powershell module to PSGallery
+task Publish-PyPs -If { ShouldPublish } Assemble-PyPs, {
+    assert($PsGalleryApiKey) "PSGALLERY_API_KEY not set"
+
+    $artifact = $(Join-Path -Path $workspace -ChildPath $module_name -Resolve)
+    assert(Test-Path $artifact) "artifact missing - run Invoke-Build Assemble"
+
+    Publish-Module `
+        -Path $artifact `
+        -NuGetApiKey $PsGalleryApiKey `
+        -Repository PSGallery `
+}
+
+$profile_contents = 'dotfiles', 'tools'
+
+# Synopsis: assemble profile as nuget package
+task Assemble-Profile -Inputs { Get-ChildItem $profile_contents -Recurse -File } -Outputs { "$workspace/$package_name.$Version.nupkg" } Init, {
     $target = Join-Path -Path $workspace -ChildPath $package_name
     EnsureExistsAndEmpty $target
 
-    # gather contents in temp folder
-    $contents = 'dotfiles', 'tools' | ForEach-Object { Join-Path -Path $PSScriptRoot -ChildPath $_ -Resolve }
-    Copy-Item -Path $contents -Destination $target -Recurse -Exclude *.Tests.*
+    # gather contents in dist folder
+    Copy-Item -Path $profile_contents -Destination $target -Recurse -Exclude *.Tests.*
     Copy-Item -Path 'py-profile.nuspec' -Destination $target
 
     # package it
-    choco pack $(Join-Path -Path $target -ChildPath 'py-profile.nuspec' -Resolve) --version=$version --out $workspace
+    choco pack $(Join-Path -Path $target -ChildPath 'py-profile.nuspec' -Resolve) --version=$Version --out $workspace
+}
+
+# Synopsis: push nuget pkg to bintray feed
+task Publish-Profile -If { ShouldPublish } Assemble-Profile, {
+    assert($BintrayApiKey) "BINTRAY_API_KEY not set"
+
+    # ignore error on add as it may already be configured
+    nuget sources Add -Name bintray -Source https://api.bintray.com/nuget/pyranja/py-get -UserName pyranja -Password $BintrayApiKey -NonInteractive
+    $spec = Join-Path $workspace "$package_name\$package_name.nuspec" -Resolve
+    exec { nuget push $spec -Source https://api.bintray.com/nuget/pyranja/py-get -ApiKey "pyranja:$BintrayApiKey" -NonInteractive }
 }
 
 # Synopsis: meta task to exec each individual assemble
 task Assemble Assemble-Profile,Assemble-PyPs, {}
+
+# Synopsis: meta task to exec each individual publish
+task Publish Publish-PyPs,Publish-Profile, {}
 
 # Synopsis: run unit tests
 task Test Init, {
@@ -107,10 +144,19 @@ function ReportTestResults {
 function EnsureExistsAndEmpty {
     <#
     .SYNOPSIS
-    ensure the given $target folder exists and is empty
+    Ensure the given $target folder exists and is empty
     #>
     Param([string] $target)
 
     Remove-Item -Path $target -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
     New-Item -Path $target -ItemType Directory -Force | Out-Null
+}
+
+function ShouldPublish {
+    <#
+    .SYNOPSIS
+    Check if a release build is running
+    #>
+    $isReleaseBuild = ($Env:APPVEYOR -eq 'True') -and ($Env:APPVEYOR_REPO_BRANCH -eq 'master') -and ($Env:APPVEYOR_PULL_REQUEST_NUMBER -eq 0)
+    ($ForcePublish) -or ($isReleaseBuild)
 }
